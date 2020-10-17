b0ef524b534f04e8d8f0605c90e0428bb77e1586
Apoorva K <320087877@INGBTCPIC5NBW71>
2020-10-17 23:55:41 +0530

    email alerts to registered users

---
+++ AssistPurchaseBackend/Controllers/AlertUserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AssistPurchaseBackend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssistPurchaseBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertUserController : ControllerBase
+    {
+        private AlertUser user = new AlertUser();
+
+        [HttpGet("Registration/{username}/{usermaildid}/{productbooked}/{UserCallid}")]
+        public List<UserDetails> AddUserDetailsforRegistration(string username,string usermaildid,string productbooked,int UserCallid)
+        {
+            return user.UserRegistration(username,usermaildid,productbooked,UserCallid);
+        }
+        [HttpPost("NewModelAlert")]
+        public string NewModelEmailAlerttoUser()
+        {
+
+            return user.NewModelEmailAlert();
+        }
+        [HttpPost("OrderConfirmation/{registeredname}/{productsbooked}")]
+        public string OrderConfirmationEmailAlerttoRegisteredUsers(string registeredname,string productsbooked)
+        {
+
+            return user.OrderConfirmationEmailAlert(registeredname,productsbooked);
+        }
+        [HttpPost("OrderConfirmation/{registeredname}/{RegisteredUserphoneno}")]
+        public string CallBackRequestFromRegisteredUser(string registeredname, int RegisteredUserphoneno)
+        {
+
+            return user.UserCallBackRequest(registeredname,RegisteredUserphoneno);
+        }
+    }
+}
--- AssistPurchaseBackend/Services/AlertUser.cs
+++ AssistPurchaseBackend/Services/AlertUser.cs
@@ -3,17 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AssistPurchaseBackend.Services
 {
     public class AlertUser
     {
-        List<IAlertUser> _userDetails=new List<IAlertUser>();
-        private IAlertUser user;
+        private string message;
+        List<UserDetails> _userDetails = new List<UserDetails>();
 
-        public void UserRegistration()
+        UserDetails user = new UserDetails();
+
+        public List<UserDetails> UserRegistration(string username, string usermailid, string bookedproduct,
+            int UserPhoneNo)
+        {
+            _userDetails.Add(
+                new UserDetails
+                {
+                    UserName = username,
+                    UserEmailID = usermailid,
+                    ProductsBooked = bookedproduct,
+                    UserContactNo = UserPhoneNo
+
+                });
+            return _userDetails;
+        }
+
+        public string NewModelEmailAlert()
+        {
+           
+            if (_userDetails != null)
+            {
+                message = "A new Model has arrived!!!!!";
+            }
+            else
+            {
+                message = "No users registered to alert";
+            }
+
+            return message;
+        }
+
+        public string UserCallBackRequest(string registeredusername, int phoneno)
         {
             
+            foreach (var userdetailslist in _userDetails)
+            {
+                if (registeredusername == userdetailslist.UserName && phoneno == userdetailslist.UserContactNo)
+                {
+                    message = "One of our Philips Personnel will reach you out soon..Thank You!!!";
+                }
+                else
+                {
+                    message = "User not registered or registered phone no doesnot match";
+                }
+            }
+
+
+
+            return message;
+        }
+
+        public string OrderConfirmationEmailAlert(string registereduser, string confirmproductbooking)
+        {
+
+            foreach (var userdetailslist in _userDetails)
+            {
+               
+                if (registereduser == userdetailslist.UserName &&
+                    confirmproductbooking == userdetailslist.ProductsBooked)
+                {
+                    message = $"{registereduser} has booked the following product {confirmproductbooking} ";
+                }
+                else
+                {
+                    message = "User not registered or device name doesnot match";
+                }
+
+               
+            }
+            return message;
         }
     }
 }
