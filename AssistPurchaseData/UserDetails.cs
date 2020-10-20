071ed964290caa0a5151cde5af3bf247b58c01f2
Maurya <Aruna.Maurya@philips.com>
2020-10-20 13:56:26 +0530

    Resolving resharper errors

--- AssistPurchaseData/UserDetails.cs
+++ AssistPurchaseData/UserDetails.cs
@@ -4,7 +4,7 @@
     {
         
         public  string UserName { get; set; }
-        public string UserEmailId { get; set; }
+        public string UserEmailId { get; }
         public string ProductsBooked { get; set; }
         public int UserContactNo { get; set; }
     }
